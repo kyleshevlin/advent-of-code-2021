ed50ca4f07b19898a2783748d4fd26aa81875c72
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2021-12-01 21:41:31 -0800

    adds solution for the second part of day 1

--- src/day-01/index.js
+++ src/day-01/index.js
@@ -7,6 +7,33 @@ const data = fs.readFileSync(path.resolve(__dirname, './input.txt'), {
 
 const numbers = data.trim().split('\n').map(Number)
 
+/**
+ * Named after the purpose in the puzzle, this function takes an array of
+ * numbers and sums 3 numbers at a time in a sliding window. So the first
+ * sum is of indexes 0, 1, and 2, the next sum is indexes 1, 2, 3, the next
+ * sum is indexes 2, 3, 4 and so on.
+ */
+function convertToSlidingWindows(numbers) {
+  let [a, b, ...otherNumbers] = numbers
+  let c
+  const sums = []
+
+  for (const number of otherNumbers) {
+    c = number
+    const sum = a + b + c
+    sums.push(sum)
+
+    a = b
+    b = c
+  }
+
+  return sums
+}
+
+/**
+ * This function receives an array of numbers and determines if the current
+ * value is greater than the previous value, incrementing a count each time
+ */
 function increasingValuesCount(values) {
   const [firstValue, ...otherValues] = values
   let count = 0
@@ -20,6 +47,7 @@ function increasingValuesCount(values) {
   return count
 }
 
-const answer = increasingValuesCount(numbers) // 1722
+const firstAnswer = increasingValuesCount(numbers) // 1722
+const secondAnswer = increasingValuesCount(convertToSlidingWindows(numbers)) // 1748
 
-module.exports = increasingValuesCount
+module.exports = { convertToSlidingWindows, increasingValuesCount }
--- src/day-01/index.test.js
+++ src/day-01/index.test.js
@@ -1,6 +1,11 @@
-const increasingValuesCount = require('./')
+const { convertToSlidingWindows, increasingValuesCount } = require('./')
 
 test('increasingValuesCount', () => {
   const numbers = [1, 2, 3, 1, 2, 3, 1, 2, 3]
   expect(increasingValuesCount(numbers)).toEqual(6)
+})
+
+test('convertToSlidingWindows', () => {
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  expect(convertToSlidingWindows(numbers)).toEqual([6, 9, 12, 15, 18, 21, 24])
 })
