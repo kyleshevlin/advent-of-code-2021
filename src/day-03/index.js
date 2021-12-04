d714b16998591303a4b7ccf369e9d1334076c279
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2021-12-03 17:47:00 -0800

    solution for day 3 part 2

--- src/day-03/index.js
+++ src/day-03/index.js
@@ -32,8 +32,6 @@ function getBitCountsPerColumn(rows) {
 
 const counts = getBitCountsPerColumn(bits)
 
-console.log(counts, Object.values(counts))
-
 function getWinner(count) {
   const [zero, one] = Object.values(count)
   return zero > one ? '0' : '1'
@@ -54,8 +52,59 @@ const epsilonStr = flipBitStr(gammaStr)
 const gamma = parseInt(gammaStr, 2)
 const epsilon = parseInt(epsilonStr, 2)
 
-const firstAnswer = gamma * epsilon
+const firstAnswer = gamma * epsilon // 3901196
 
-console.log(firstAnswer)
+function getOxygenCountIndex(count) {
+  const [zero, one] = Object.values(count)
 
-module.exports = { flipBitStr, getBitCountsPerColumn, getWinner }
+  if (zero > one) return '0'
+  return '1'
+}
+
+function getCO2CountIndex(count) {
+  const [zero, one] = Object.values(count)
+
+  if (one < zero) return '1'
+  return '0'
+}
+
+function getRating(rows, getCountIndex) {
+  let currentRows = rows
+  let currentCounts = getBitCountsPerColumn(rows)
+  let colIdx = 0
+  const cols = rows[0].length
+  let result
+
+  while (colIdx < cols && !result) {
+    const count = currentCounts[colIdx]
+    const countIdx = getCountIndex(count)
+
+    currentRows = currentRows.filter(row => row[colIdx] === countIdx)
+
+    if (currentRows.length === 1) {
+      result = currentRows[0]
+      return result
+    }
+
+    currentCounts = getBitCountsPerColumn(currentRows)
+    colIdx++
+  }
+
+  return result
+}
+
+const oxygenBitStr = getRating(bits, getOxygenCountIndex).join('')
+const co2BitStr = getRating(bits, getCO2CountIndex).join('')
+const oxygen = parseInt(oxygenBitStr, 2)
+const co2 = parseInt(co2BitStr, 2)
+
+const secondAnswer = oxygen * co2 // 4412188
+
+module.exports = {
+  flipBitStr,
+  getBitCountsPerColumn,
+  getCO2CountIndex,
+  getOxygenCountIndex,
+  getRating,
+  getWinner,
+}
--- src/day-03/index.test.js
+++ src/day-03/index.test.js
@@ -1,4 +1,11 @@
-const { flipBitStr, getBitCountsPerColumn, getWinner } = require('./')
+const {
+  flipBitStr,
+  getBitCountsPerColumn,
+  getCO2CountIndex,
+  getOxygenCountIndex,
+  getRating,
+  getWinner,
+} = require('./')
 
 test('flipBitStr', () => {
   expect(flipBitStr('001010110')).toEqual('110101001')
@@ -34,4 +41,36 @@ test('getBitCountsPerColumn', () => {
 test('getWinner', () => {
   expect(getWinner({ 0: 400, 1: 500 })).toEqual('1')
   expect(getWinner({ 0: 500, 1: 400 })).toEqual('0')
+})
+
+test('getOxygenCountIndex', () => {
+  expect(getOxygenCountIndex({ 0: 400, 1: 500 })).toEqual('1')
+  expect(getOxygenCountIndex({ 0: 500, 1: 400 })).toEqual('0')
+  expect(getOxygenCountIndex({ 0: 400, 1: 400 })).toEqual('1')
+})
+
+test('getCO2CountIndex', () => {
+  expect(getCO2CountIndex({ 0: 400, 1: 500 })).toEqual('0')
+  expect(getCO2CountIndex({ 0: 500, 1: 400 })).toEqual('1')
+  expect(getCO2CountIndex({ 0: 400, 1: 400 })).toEqual('0')
+})
+
+test('getRating', () => {
+  const bits = [
+    '00100',
+    '11110',
+    '10110',
+    '10111',
+    '10101',
+    '01111',
+    '00111',
+    '11100',
+    '10000',
+    '11001',
+    '00010',
+    '01010',
+  ].map(str => str.split(''))
+
+  expect(getRating(bits, getOxygenCountIndex).join('')).toEqual('10111')
+  expect(getRating(bits, getCO2CountIndex).join('')).toEqual('01010')
 })
