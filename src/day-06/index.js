3e72fcc3b17535e1e9c1bb057c78393e8533198a
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2021-12-05 22:24:07 -0800

    adds solution for day 6 part 2

--- src/day-06/index.js
+++ src/day-06/index.js
@@ -40,6 +40,45 @@ for (let i = 0; i < 80; i++) {
 
 const firstAnswer = firstSim.getFish().length // 372300
 
+function optimizedSimulation(startingFish) {
+  const fishCounts = { 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0 }
+  startingFish.forEach(f => {
+    fishCounts[f]++
+  })
+
+  const getFish = () => fishCounts
+
+  function tick() {
+    let tmpZeroCount = fishCounts[0]
+
+    for (let i = 1; i < 9; i++) {
+      fishCounts[i - 1] = fishCounts[i]
+    }
+
+    fishCounts[8] = tmpZeroCount
+    fishCounts[6] += tmpZeroCount
+
+    return fishCounts
+  }
+
+  return {
+    getFish,
+    tick,
+  }
+}
+
+const secondSim = optimizedSimulation(fishData)
+
+for (let i = 0; i < 256; i++) {
+  secondSim.tick()
+}
+
+const secondAnswer = Object.values(secondSim.getFish()).reduce(
+  (acc, cur) => acc + cur,
+  0
+) // 1675781200288
+
 module.exports = {
   simulation,
+  optimizedSimulation,
 }
--- src/day-06/index.test.js
+++ src/day-06/index.test.js
@@ -1,4 +1,4 @@
-const { simulation } = require('./')
+const { simulation, optimizedSimulation } = require('./')
 
 test('simulation', () => {
   const sim = simulation([3])
@@ -16,4 +16,40 @@ test('simulation', () => {
   expect(sim.tick()).toEqual([6, 8, 1])
   expect(sim.tick()).toEqual([5, 7, 0])
   expect(sim.tick()).toEqual([4, 6, 6, 8])
+})
+
+const emptyFish = {
+  0: 0,
+  1: 0,
+  2: 0,
+  3: 0,
+  4: 0,
+  5: 0,
+  6: 0,
+  7: 0,
+  8: 0,
+}
+
+const mergeFish = (partial = {}) => ({
+  ...emptyFish,
+  ...partial,
+})
+
+test('optimizedSimulation', () => {
+  const sim = optimizedSimulation([3])
+
+  expect(sim.getFish()).toEqual(mergeFish({ 3: 1 }))
+  expect(sim.tick()).toEqual(mergeFish({ 2: 1 }))
+  expect(sim.tick()).toEqual(mergeFish({ 1: 1 }))
+  expect(sim.tick()).toEqual(mergeFish({ 0: 1 }))
+  expect(sim.tick()).toEqual(mergeFish({ 6: 1, 8: 1 }))
+  expect(sim.tick()).toEqual(mergeFish({ 5: 1, 7: 1 }))
+  expect(sim.tick()).toEqual(mergeFish({ 4: 1, 6: 1 }))
+  expect(sim.tick()).toEqual(mergeFish({ 3: 1, 5: 1 }))
+  expect(sim.tick()).toEqual(mergeFish({ 2: 1, 4: 1 }))
+  expect(sim.tick()).toEqual(mergeFish({ 1: 1, 3: 1 }))
+  expect(sim.tick()).toEqual(mergeFish({ 0: 1, 2: 1 }))
+  expect(sim.tick()).toEqual(mergeFish({ 1: 1, 6: 1, 8: 1 }))
+  expect(sim.tick()).toEqual(mergeFish({ 0: 1, 5: 1, 7: 1 }))
+  expect(sim.tick()).toEqual(mergeFish({ 4: 1, 6: 2, 8: 1 }))
 })
