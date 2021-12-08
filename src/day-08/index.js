4b0687043be4d665bff8e422815dfe23a266d88f
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2021-12-07 23:11:06 -0800

    solution for day 8 part 2

--- src/day-08/index.js
+++ src/day-08/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const path = require('path')
+const { decode } = require('punycode')
 
 const data = fs.readFileSync(path.resolve(__dirname, './input.txt'), {
   encoding: 'utf-8',
@@ -39,6 +40,111 @@ function simpleNumberCount(patterns) {
 
 const firstAnswer = main(signalData) // 288
 
+const getNumber = length => patterns => patterns.find(p => p.length === length)
+const getOne = getNumber(2)
+const getSeven = getNumber(3)
+const getFour = getNumber(4)
+const getEight = getNumber(7)
+
+function partTwo(signals) {
+  const sortedSignals = signals.map(([patterns, outputs]) => {
+    const sortedPatterns = patterns.map(p => [...p].sort().join(''))
+    const sortedOutputs = outputs.map(o => [...o].sort().join(''))
+
+    return [sortedPatterns, sortedOutputs]
+  })
+
+  const decodedOutputValues = sortedSignals.map(signal =>
+    decipherOutput(signal)
+  )
+
+  // console.log(decodedOutputValues)
+
+  const result = decodedOutputValues.reduce((acc, cur) => acc + cur, 0)
+
+  return result
+}
+
+/**
+ * Well, it works 😅
+ */
+function decipherOutput([patterns, output]) {
+  const one = getOne(patterns)
+  const seven = getSeven(patterns)
+  const four = getFour(patterns)
+  const eight = getEight(patterns)
+
+  const knownSegments = {
+    top: null,
+    upperRight: null,
+    lowerRight: null,
+  }
+
+  // We can know the top for certain because we can remove the segments of one
+  // from seven
+  knownSegments.top = [...seven].filter(l => !one.includes(l))[0]
+
+  // 6 is the only number with 6 segments, and only one of the segments of one
+  const six = patterns.filter(
+    pattern =>
+      pattern.length === 6 &&
+      [...one].filter(l => pattern.includes(l)).length === 1
+  )[0]
+
+  knownSegments.upperRight = [...one].filter(l => !six.includes(l))[0]
+  knownSegments.lowerRight = [...one].filter(
+    l => l !== knownSegments.upperRight
+  )[0]
+
+  // Three is the only one of length 5 with both the segments of one
+  const three = patterns.filter(
+    pattern =>
+      pattern.length === 5 &&
+      pattern.includes(knownSegments.upperRight) &&
+      pattern.includes(knownSegments.lowerRight)
+  )[0]
+
+  // Two is of length 5, isn't three (which has the lower right segment) and
+  // has the upper right segment
+  const two = patterns.filter(
+    pattern =>
+      pattern.length === 5 &&
+      pattern !== three &&
+      pattern.includes(knownSegments.upperRight)
+  )[0]
+
+  // Five is of length 5 and isn't two and three
+  const five = patterns.filter(
+    pattern => pattern.length === 5 && pattern !== two && pattern !== three
+  )[0]
+
+  // Nine is different than zero because it contains the middle (all the
+  // segments of four) and zero is missing one of four's segments
+  const nine = patterns.filter(
+    pattern => pattern.length === 6 && [...four].every(l => pattern.includes(l))
+  )[0]
+
+  const decipheredOutputs = output.map(val => {
+    if (val === one) return 1
+    if (val === two) return 2
+    if (val === three) return 3
+    if (val === four) return 4
+    if (val === five) return 5
+    if (val === six) return 6
+    if (val === seven) return 7
+    if (val === eight) return 8
+    if (val === nine) return 9
+
+    return 0
+  })
+
+  return Number(decipheredOutputs.join(''))
+}
+
+const secondAnswer = partTwo(signalData) // 940724
+
 module.exports = {
+  decipherOutput,
   main,
+  partTwo,
 }
--- src/day-08/index.test.js
+++ src/day-08/index.test.js
@@ -1,22 +1,42 @@
-const { main } = require('./')
+const { decipherOutput, partTwo } = require('./')
 
-test('main', () => {
+test('decipherOutput', () => {
   const data = [
-    'acedgfb',
-    'cdfbe',
-    'gcdfa',
-    'fbcad',
-    'dab',
-    'cefabd',
-    'cdfgeb',
-    'eafb',
-    'cagedb',
-    'ab',
-    '|',
-    'cdfeb',
-    'fcadb',
-    'cdfeb',
-    'cdbaf',
+    [
+      'acedgfb',
+      'cdfbe',
+      'gcdfa',
+      'fbcad',
+      'dab',
+      'cefabd',
+      'cdfgeb',
+      'eafb',
+      'cagedb',
+      'ab',
+    ].map(p => [...p].sort().join('')),
+    ['cdfeb', 'fcadb', 'cdfeb', 'cdbaf'].map(o => [...o].sort().join('')),
   ]
-  expect(main(data)).toEqual([])
+  expect(decipherOutput(data)).toEqual(5353)
+})
+
+test('partTwo', () => {
+  const data = `
+be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
+`
+  const signalData = data
+    .trim()
+    .split('\n')
+    .map(line => line.split(' | '))
+    .map(([patterns, output]) => [patterns.split(' '), output.split(' ')])
+
+  expect(partTwo(signalData)).toEqual(61229)
 })
