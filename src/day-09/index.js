e63ce399f8e56f600dcccea0c299bf6c4a909cd7
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2021-12-08 23:36:16 -0800

    solution for day 9 part 2

--- src/day-09/index.js
+++ src/day-09/index.js
@@ -40,6 +40,71 @@ function partOne(map) {
 
 const firstAnswer = partOne(data) // 423
 
+const makePoint = (rowIdx, colIdx) => [rowIdx, colIdx]
+const p = makePoint
+
+function partTwo(map) {
+  const matrix = createMatrix(map)
+
+  // We're going to store the coordinates of low points this time
+  const lowPoints = []
+  for (const [rowIdx, row] of matrix.entries()) {
+    for (const [colIdx, col] of row.entries()) {
+      const above = rowIdx > 0 ? matrix[rowIdx - 1][colIdx] : Infinity
+      const right =
+        colIdx < row.length - 1 ? matrix[rowIdx][colIdx + 1] : Infinity
+      const below =
+        rowIdx < matrix.length - 1 ? matrix[rowIdx + 1][colIdx] : Infinity
+      const left = colIdx > 0 ? matrix[rowIdx][colIdx - 1] : Infinity
+
+      if (col < above && col < right && col < below && col < left) {
+        lowPoints.push(p(rowIdx, colIdx))
+      }
+    }
+  }
+
+  const visited = {}
+  function walkBasin(point) {
+    const [rowIdx, colIdx] = point
+    const key = `${rowIdx}-${colIdx}`
+
+    if (visited[key]) return []
+
+    visited[key] = true
+
+    const row = matrix[rowIdx]
+
+    if (!row) return []
+
+    const col = row[colIdx]
+
+    if (col === undefined || col === 9) return []
+
+    return [
+      col,
+      ...walkBasin(p(rowIdx + 1, colIdx)),
+      ...walkBasin(p(rowIdx, colIdx + 1)),
+      ...walkBasin(p(rowIdx - 1, colIdx)),
+      ...walkBasin(p(rowIdx, colIdx - 1)),
+    ]
+  }
+
+  const basins = lowPoints.map(lowPoint => walkBasin(lowPoint))
+  const sizes = basins.map(basin => basin.length)
+  const sortedSizes = [...sizes].sort((a, b) => {
+    if (a < b) return 1
+    if (a > b) return -1
+    return 0
+  })
+  const threeLargest = sortedSizes.slice(0, 3)
+  const product = threeLargest.reduce((acc, cur) => acc * cur, 1)
+
+  return product
+}
+
+const secondAnswer = partTwo(data) // 1198704
+
 module.exports = {
   partOne,
+  partTwo,
 }
--- src/day-09/index.test.js
+++ src/day-09/index.test.js
@@ -1,4 +1,4 @@
-const { partOne } = require('./')
+const { partOne, partTwo } = require('./')
 
 test('partOne', () => {
   const data = `
@@ -10,4 +10,16 @@ test('partOne', () => {
 `
 
   expect(partOne(data)).toEqual(15)
+})
+
+test('partTwo', () => {
+  const data = `
+2199943210
+3987894921
+9856789892
+8767896789
+9899965678
+`
+
+  expect(partTwo(data)).toEqual(1134)
 })
