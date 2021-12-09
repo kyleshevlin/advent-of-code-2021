6df0e05b8bfea1dd508cfc21cc07f164a95403a5
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2021-12-09 08:09:26 -0800

    seriously don't know how that import got there

--- src/day-09/index.js
+++ src/day-09/index.js
@@ -1,6 +1,5 @@
 const fs = require('fs')
 const path = require('path')
-const { decode } = require('punycode')
 
 const data = fs.readFileSync(path.resolve(__dirname, './input.txt'), {
   encoding: 'utf-8',
