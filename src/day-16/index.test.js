d3cd08cb672ca9b2ae188d2f44724605b9e15ef4
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2021-12-13 14:58:22 -0800

    prep for the rest of the days

---
+++ src/day-16/index.test.js
@@ -0,0 +1,11 @@
+const { partOne, partTwo } = require('./')
+
+const data = ``
+
+test('partOne', () => {
+  expect(partOne(data)).toEqual()
+})
+
+test('partTwo', () => {
+  expect(partTwo(data)).toEqual()
+})
---
+++ src/day-20/index.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+const path = require('path')
+
+const data = fs.readFileSync(path.resolve(__dirname, './input.txt'), {
+  encoding: 'utf-8',
+})
+
+function partOne(input) {}
+
+function partTwo(input) {}
+
+module.exports = {
+  partOne,
+  partTwo,
+}
