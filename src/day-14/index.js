e5bede8aa52e25f170686c17cf24548349efe917
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2021-12-13 21:26:31 -0800

    solution for day 14 part 1

--- src/day-14/index.js
+++ src/day-14/index.js
@@ -5,7 +5,74 @@ const data = fs.readFileSync(path.resolve(__dirname, './input.txt'), {
   encoding: 'utf-8',
 })
 
-function partOne(input) {}
+function parseInput(input) {
+  let [polymerTemplate, pairInsertions] = input.trim().split('\n\n')
+  pairInsertions = pairInsertions.split('\n').map(item => item.split(' -> '))
+  pairInsertions = pairInsertions.reduce((acc, [pair, insertion]) => {
+    acc[pair] = insertion
+    return acc
+  }, {})
+
+  return [polymerTemplate, pairInsertions]
+}
+
+function applyInsertions(string, insertions) {
+  const pairs = []
+  for (let i = 0; i < string.length - 1; i++) {
+    pairs.push(string[i] + string[i + 1])
+  }
+
+  const inserted = pairs.map(pair => {
+    if (insertions[pair]) {
+      return pair[0] + insertions[pair] + pair[1]
+    }
+  })
+
+  const result = inserted.reduce((acc, cur) => {
+    return acc + cur.slice(1)
+  })
+
+  return result
+}
+
+function countChars(string) {
+  return [...string].reduce((acc, cur) => {
+    if (!acc[cur]) {
+      acc[cur] = 0
+    }
+
+    acc[cur]++
+
+    return acc
+  }, {})
+}
+
+function partOne(input) {
+  const [polymerTemplate, pairInsertions] = parseInput(input)
+  let polymer = polymerTemplate
+
+  for (let i = 0; i < 10; i++) {
+    polymer = applyInsertions(polymer, pairInsertions)
+  }
+
+  const counts = countChars(polymer)
+  const [, max] = Object.entries(counts).reduce(
+    (acc, cur) => {
+      return cur[1] > acc[1] ? cur : acc
+    },
+    [, -Infinity]
+  )
+  const [, min] = Object.entries(counts).reduce(
+    (acc, cur) => {
+      return cur[1] < acc[1] ? cur : acc
+    },
+    [, Infinity]
+  )
+
+  return max - min
+}
+
+const firstAnswer = partOne(data) // 3230
 
 function partTwo(input) {}
 
--- src/day-14/index.test.js
+++ src/day-14/index.test.js
@@ -1,11 +1,30 @@
 const { partOne, partTwo } = require('./')
 
-const data = ``
+const data = `
+NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C
+`
 
 test('partOne', () => {
-  expect(partOne(data)).toEqual()
+  expect(partOne(data)).toEqual(1588)
 })
 
-test('partTwo', () => {
+xtest('partTwo', () => {
   expect(partTwo(data)).toEqual()
 })
