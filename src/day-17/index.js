8d45b5d1be34f4db4407ba617a74f2428a00532d
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2021-12-18 10:25:26 -0800

    solution for day 17 part 1

--- src/day-17/index.js
+++ src/day-17/index.js
@@ -5,7 +5,94 @@ const data = fs.readFileSync(path.resolve(__dirname, './input.txt'), {
   encoding: 'utf-8',
 })
 
-function partOne(input) {}
+function parseTarget(input) {
+  const result = input
+    .replace('target area: ', '')
+    .split(', ')
+    .map(range => range.split('=')[1].split('..').map(Number))
+
+  return result
+}
+
+function launchProbe(vx, vy, target) {
+  let status = 'failure'
+  const [xRange, yRange] = target
+  let x = 0
+  let y = 0
+  let maxY = 0
+
+  function step() {
+    x += vx
+    y += vy
+    maxY = Math.max(maxY, y)
+
+    vx = Math.max(vx - 1, 0)
+    vy--
+  }
+
+  do {
+    step()
+
+    if (x >= xRange[0] && x <= xRange[1] && y >= yRange[0] && y <= yRange[1]) {
+      status = 'success'
+    }
+  } while (x <= xRange[1] && y >= yRange[0])
+
+  return { maxY, status }
+}
+
+function getMinVx(targetX) {
+  let result = 0
+  let reached = false
+
+  while (!reached) {
+    result++
+    let x = 0
+    let vx = result
+
+    const step = () => {
+      x += vx
+      vx = Math.max(vx - 1, 0)
+    }
+
+    while (x < targetX && vx !== 0) {
+      step()
+      if (x >= targetX) {
+        reached = true
+      }
+    }
+  }
+
+  return result
+}
+
+function partOne(input) {
+  const target = parseTarget(input)
+  const minVx = getMinVx(target[0][0])
+  const maxVx = target[0][1]
+
+  const solutions = []
+  for (let i = minVx; i <= maxVx; i++) {
+    for (let j = 0; j <= 1000; j++) {
+      const result = launchProbe(i, j, target)
+      if (result.status === 'success') {
+        solutions.push({ maxY: result.maxY, vx: i, vy: j })
+      }
+    }
+  }
+
+  const best = solutions.reduce(
+    (acc, cur) => {
+      if (cur.maxY > acc.maxY) return cur
+      return acc
+    },
+    { maxY: -Infinity }
+  )
+
+  return best.maxY
+}
+
+const firstAnswer = partOne(data) // 4753
 
 function partTwo(input) {}
 
--- src/day-17/index.test.js
+++ src/day-17/index.test.js
@@ -1,11 +1,11 @@
 const { partOne, partTwo } = require('./')
 
-const data = ``
+const data = `target area: x=20..30, y=-10..-5`
 
 test('partOne', () => {
-  expect(partOne(data)).toEqual()
+  expect(partOne(data)).toEqual(45)
 })
 
-test('partTwo', () => {
+xtest('partTwo', () => {
   expect(partTwo(data)).toEqual()
 })
