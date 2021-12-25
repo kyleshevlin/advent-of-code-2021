f442e39b38ae7da3da3047c93252ce1547492313
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2021-12-24 16:30:38 -0800

    solutions for day 20

--- src/day-20/index.js
+++ src/day-20/index.js
@@ -5,11 +5,102 @@ const data = fs.readFileSync(path.resolve(__dirname, './input.txt'), {
   encoding: 'utf-8',
 })
 
-function partOne(input) {}
+function parseInput(input) {
+  const [algorithm, rawImage] = input.split('\n\n').map(str => str.trim())
+  const image = rawImage.trim().split('\n')
 
-function partTwo(input) {}
+  return { algorithm, image }
+}
+
+function countLightPixels(image) {
+  return image
+    .join('')
+    .split('')
+    .filter(p => p === '#').length
+}
+
+function getPixelStr(row, col, image, defaultChar) {
+  const nw = image[row - 1]?.[col - 1] ?? defaultChar
+  const n = image[row - 1]?.[col] ?? defaultChar
+  const ne = image[row - 1]?.[col + 1] ?? defaultChar
+  const w = image[row]?.[col - 1] ?? defaultChar
+  const c = image[row]?.[col] ?? defaultChar
+  const e = image[row]?.[col + 1] ?? defaultChar
+  const sw = image[row + 1]?.[col - 1] ?? defaultChar
+  const s = image[row + 1]?.[col] ?? defaultChar
+  const se = image[row + 1]?.[col + 1] ?? defaultChar
+
+  const pixels = [nw, n, ne, w, c, e, sw, s, se]
+  const result = pixels.join('')
+
+  return result
+}
+
+function getBitStr(pixelStr) {
+  return [...pixelStr]
+    .map(char => {
+      if (char === '.') return 0
+      if (char === '#') return 1
+    })
+    .join('')
+}
+
+function enhanceImage(algorithm, image, defaultChar = '.') {
+  const result = []
+
+  for (let row = -1; row < image.length + 1; row++) {
+    let line = ''
+
+    for (let col = -1; col < image[0].length + 1; col++) {
+      const pixelStr = getPixelStr(row, col, image, defaultChar)
+      const bitStr = getBitStr(pixelStr)
+      const index = parseInt(bitStr, 2)
+      const char = algorithm[index]
+
+      line += char
+    }
+
+    result.push(line)
+  }
+
+  return result
+}
+
+function solve(input, steps) {
+  const { algorithm, image } = parseInput(input)
+
+  let enhancedImage = image
+  for (let i = 0; i < steps; i++) {
+    // This edge case is honestly bullshit
+    let defaultChar = '.'
+
+    if (algorithm[0] === '#') {
+      defaultChar = i % 2 ? '#' : '.'
+    }
+
+    enhancedImage = enhanceImage(algorithm, enhancedImage, defaultChar)
+  }
+
+  const lightPixelCount = countLightPixels(enhancedImage)
+
+  return lightPixelCount
+}
+
+function partOne(input) {
+  return solve(input, 2)
+}
+
+const firstAnswer = partOne(data) // 5316
+
+function partTwo(input) {
+  return solve(input, 50)
+}
+
+const secondAnswer = partTwo(data) // 16728
 
 module.exports = {
+  enhanceImage,
+  parseInput,
   partOne,
   partTwo,
 }
--- src/day-20/index.test.js
+++ src/day-20/index.test.js
@@ -1,11 +1,55 @@
-const { partOne, partTwo } = require('./')
+const {
+  enhanceImage,
+  matrixToRawImage,
+  parseInput,
+  partOne,
+  partTwo,
+} = require('./')
 
-const data = ``
+const data = `
+..#.#..#####.#.#.#.###.##.....###.##.#..###.####..#####..#....#..#..##..###..######.###...####..#..#####..##..#.#####...##.#.#..#.##..#.#......#.###.######.###.####...#.##.##..#..#..#####.....#.#....###..#.##......#.....#..#..#..##..#...##.######.####.####.#.#...#.......#..#.#.#...####.##.#......#..#...##.#.##..#...##.#.##..###.#......#.#.......#.#.#.####.###.##...#.....####.#..#..#.##.#....##..#.####....##...##..#...#......#.#.......#.......##..####..#...#.#.#...##..#.#..###..#####........#..####......#..#
+
+#..#.
+#....
+##..#
+..#..
+..###
+`
 
 test('partOne', () => {
-  expect(partOne(data)).toEqual()
+  expect(partOne(data)).toEqual(35)
 })
 
 test('partTwo', () => {
-  expect(partTwo(data)).toEqual()
+  expect(partTwo(data)).toEqual(3351)
+})
+
+test('enhanceImage', () => {
+  const { algorithm, image } = parseInput(data)
+  const result = enhanceImage(algorithm, image)
+
+  expect(result).toEqual([
+    '.##.##.',
+    '#..#.#.',
+    '##.#..#',
+    '####..#',
+    '.#..##.',
+    '..##..#',
+    '...#.#.',
+  ])
+
+  const edgeCaseAlgo = '#' + '.'.repeat(511)
+  const result2 = enhanceImage(edgeCaseAlgo, ['...', '...', '...'])
+  expect(result2).toEqual(['#####', '#####', '#####', '#####', '#####'])
+
+  const result3 = enhanceImage(edgeCaseAlgo, result2)
+  expect(result3).toEqual([
+    '.......',
+    '.......',
+    '.......',
+    '.......',
+    '.......',
+    '.......',
+    '.......',
+  ])
 })
