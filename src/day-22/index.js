905dbb8eb861389065e5a14c37f4a525f925b412
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2021-12-27 09:14:30 -0800

    solution for day 22 part 2

--- src/day-22/index.js
+++ src/day-22/index.js
@@ -59,11 +59,10 @@ function createBox() {
   }
 }
 
-function partOne(input) {
-  const instructions = parseInput(input)
-  // The "box" seems too big to make the full thing, even as a Map or Object
-  // So to get around this, since we know everything is off at the start, let's
-  // just track the changes the instructions give and assume the rest
+// The "box" seems too big to make the full thing, even as a Map or Object
+// So to get around this, since we know everything is off at the start, let's
+// just track the changes the instructions give and assume the rest
+function naiveSolution(instructions) {
   const box = createBox()
 
   instructions.forEach(instruction => {
@@ -73,11 +72,102 @@ function partOne(input) {
   return Object.values(box.state()).filter(val => val === 1).length
 }
 
+function partOne(input) {
+  const instructions = parseInput(input)
+  return naiveSolution(instructions)
+}
+
 const firstAnswer = partOne(data) // 623748
 
-function partTwo(input) {}
+function createCuboid({ setting, xRange, yRange, zRange }) {
+  return {
+    setting,
+    x1: xRange[0],
+    x2: xRange[1],
+    y1: yRange[0],
+    y2: yRange[1],
+    z1: zRange[0],
+    z2: zRange[1],
+  }
+}
+
+function cuboidVolume(cube) {
+  const { x1, x2, y1, y2, z1, z2 } = cube
+  return (x2 - x1 + 1) * (y2 - y1 + 1) * (z2 - z1 + 1)
+}
+
+/**
+ * This function recursively subtracts the volume of any future intersection
+ * that would overwrite its current volume. Why? Because this leaves us with
+ * only the volume that this cuboid adds to the total.
+ */
+function futureVolumeOverwrites(cuboid, otherCuboids) {
+  const volumes = otherCuboids.map((otherCuboid, idx) => {
+    const intersection = intersectingCuboid(cuboid, otherCuboid)
+
+    if (!intersection) return 0
+
+    return (
+      cuboidVolume(intersection) -
+      futureVolumeOverwrites(intersection, otherCuboids.slice(idx + 1))
+    )
+  })
+
+  const result = volumes.reduce((x, y) => x + y, 0)
+
+  return result
+}
+
+function intersectingCuboid(cubeA, cubeB) {
+  const x1 = Math.max(cubeA.x1, cubeB.x1)
+  const x2 = Math.min(cubeA.x2, cubeB.x2)
+  if (x1 > x2) return
+
+  const y1 = Math.max(cubeA.y1, cubeB.y1)
+  const y2 = Math.min(cubeA.y2, cubeB.y2)
+  if (y1 > y2) return
+
+  const z1 = Math.max(cubeA.z1, cubeB.z1)
+  const z2 = Math.min(cubeA.z2, cubeB.z2)
+  if (z1 > z2) return
+
+  return createCuboid({
+    setting: cubeB.setting,
+    xRange: [x1, x2],
+    yRange: [y1, y2],
+    zRange: [z1, z2],
+  })
+}
+
+function partTwo(input) {
+  const instructions = parseInput(input)
+  const cuboids = instructions.map(createCuboid)
+
+  let totalVolume = 0
+  for (const [idx, cuboid] of cuboids.entries()) {
+    /**
+     * Why can we skip offs? Because of the way that `futureVolumeOverwrites`
+     * works, the only volume a cuboid adds is the volume that is never
+     * overwritten. Since any portion of an off cuboid that never gets
+     * overwritten would not be counted any way, we can skip it.
+     */
+    if (cuboid.setting === 'off') continue
+
+    const additionalVolume =
+      cuboidVolume(cuboid) -
+      futureVolumeOverwrites(cuboid, cuboids.slice(idx + 1))
+
+    totalVolume += additionalVolume
+  }
+
+  return totalVolume
+}
+
+const secondAnswer = partTwo(data) // 1227345351869476
 
 module.exports = {
+  createCuboid,
+  cuboidVolume,
   partOne,
   partTwo,
 }
