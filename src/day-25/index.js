5ea4d45ce89b221d06282303b46d8d58ce267f1d
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2022-01-26 10:20:39 -0800

    solution for day 25

--- src/day-25/index.js
+++ src/day-25/index.js
@@ -5,11 +5,89 @@ const data = fs.readFileSync(path.resolve(__dirname, './input.txt'), {
   encoding: 'utf-8',
 })
 
-function partOne(input) {}
+function parseInput(input) {
+  return input
+    .trim()
+    .split('\n')
+    .map(r => r.split(''))
+}
+
+function createSim(input) {
+  const state = parseInput(input)
+
+  return {
+    tick() {
+      const horizontalOps = []
+
+      for (const [rowIdx, row] of state.entries()) {
+        for (const [colIdx, col] of row.entries()) {
+          const nextColIdx = colIdx === state[0].length - 1 ? 0 : colIdx + 1
+
+          if (col === '>' && row[nextColIdx] === '.') {
+            // create an op to swap them.
+            horizontalOps.push({ rowIdx, colIdx })
+          }
+        }
+      }
+
+      for (const op of horizontalOps) {
+        const nextColIdx = op.colIdx === state[0].length - 1 ? 0 : op.colIdx + 1
+
+        state[op.rowIdx][op.colIdx] = '.'
+        state[op.rowIdx][nextColIdx] = '>'
+      }
+
+      const verticalOps = []
+
+      for (const [rowIdx, row] of state.entries()) {
+        for (const [colIdx, col] of row.entries()) {
+          const nextRowIdx = rowIdx === state.length - 1 ? 0 : rowIdx + 1
+
+          if (col === 'v' && state[nextRowIdx][colIdx] === '.') {
+            // create an op to swap them.
+            verticalOps.push({ rowIdx, colIdx })
+          }
+        }
+      }
+
+      for (const op of verticalOps) {
+        const nextRowIdx = op.rowIdx === state.length - 1 ? 0 : op.rowIdx + 1
+
+        state[op.rowIdx][op.colIdx] = '.'
+        state[nextRowIdx][op.colIdx] = 'v'
+      }
+
+      return this
+    },
+    print() {
+      return state.map(row => row.join('')).join('\n')
+    },
+  }
+}
+
+function partOne(input) {
+  const sim = createSim(input)
+  let changed = false
+  let previous = sim.print()
+  let count = 0
+
+  do {
+    count++
+    sim.tick()
+    changed = previous !== sim.print()
+    previous = sim.print()
+  } while (changed)
+
+  return count
+}
+
+const firstAnswer = partOne(data)
+console.log(firstAnswer)
 
 function partTwo(input) {}
 
 module.exports = {
+  createSim,
   partOne,
   partTwo,
 }
--- src/day-25/index.test.js
+++ src/day-25/index.test.js
@@ -1,9 +1,68 @@
-const { partOne, partTwo } = require('./')
+const { createSim, partOne, partTwo } = require('./')
 
-const data = ``
+const data = `
+v...>>.vv>
+.vv>>.vv..
+>>.>v>...v
+>>v>>.>.v.
+v>v.vv.v..
+>.>>..v...
+.vv..>.>v.
+v.v..>>v.v
+....v..v.>
+`
+
+describe('simulations', () => {
+  it('should move eastwardly', () => {
+    const sim = createSim('...>>>>>...')
+    expect(sim.tick().print()).toEqual('...>>>>.>..')
+    expect(sim.tick().print()).toEqual('...>>>.>.>.')
+  })
+
+  it('should move eastward, than southward', () => {
+    const sim = createSim(`
+..........
+.>v....v..
+.......>..
+..........
+`)
+    expect(sim.tick().print()).toEqual(
+      `
+..........
+.>........
+..v....v>.
+..........
+`.trim()
+    )
+  })
+
+  it('should wrap around', () => {
+    const sim = createSim(`
+...>...
+.......
+......>
+v.....>
+......>
+.......
+..vvv..
+`)
+
+    expect(sim.tick().print()).toEqual(
+      `
+..vv>..
+.......
+>......
+v.....>
+>......
+.......
+....v..
+`.trim()
+    )
+  })
+})
 
 test('partOne', () => {
-  expect(partOne(data)).toEqual()
+  expect(partOne(data)).toEqual(58)
 })
 
 test('partTwo', () => {
